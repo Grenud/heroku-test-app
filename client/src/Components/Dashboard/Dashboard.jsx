f6240cbe8e99fc5d3537adf24429799b56511395
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2024-04-27 14:01:23 +0530

    Routes Added. Header Section added for ease.

---
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function Dashboard() {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get("http://localhost:8000/api/bayava")
+			.then((res) => {
+				setData(res.data.rows);
+				console.log(res.data.rows);
+			})
+			.catch((err) => console.log(err));
+	}, []);
+
+	return (
+		<div className="h-screen flex flex-wrap items-center justify-center gap-10">
+			{data.map((item, index) => {
+				const firstName = item.instructor__c.split(" ")[0];
+				return (
+					<div key={index}>
+						<div className="max-w-sm rounded overflow-hidden shadow-lg backdrop-blur-lg">
+							<img
+								className="object-cover h-48 w-96"
+								src={item.cover_photo__c}
+							/>
+							<div className="px-6 py-4">
+								<div className="flex justify-between items-center gap-3">
+									<div className="font-bold text-xl mb-2">
+										{item.name}
+									</div>
+									<div className="text-gray-700 text-base">
+										Instructor :{" "}
+										<span className="font-semibold">
+											{firstName}
+										</span>
+									</div>
+								</div>
+								<div className="flex justify-between items-center gap-3">
+									<p>Starts : {item.start_date__c}</p>
+									<p>Ends : {item.end_date__c}</p>
+								</div>
+							</div>
+							<div className="px-6 py-4 flex items-center justify-between">
+								<h3 className="text-2xl text-gray-700 font-black">
+									{item.mode__c}
+								</h3>
+								<button className="enroll-button">
+									Enroll
+								</button>
+							</div>
+						</div>
+					</div>
+				);
+			})}
+		</div>
+	);
+}
+
+export default Dashboard;
---
+++ client/src/Components/Header/Navbar.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+	return (
+		<div className="fixed top-0 left-0 z-100 w-full flex flex-col items-center justify-center">
+			<nav className="backdrop-blur-lg fixed top-0 left-0 w-full h-16 flex items-center justify-between px-10 border-b-2 shadow-lg z-100">
+				<div>
+					<Link to="/" className="logo">
+						<h1 className="">Bayava</h1>
+					</Link>
+				</div>
+				<div>
+					<ul className="flex justify-center items-center gap-7 font-lexend-exa">
+						<li>
+							<Link to="/contact">Contact</Link>
+						</li>
+						<li>
+							<Link to="/about">About</Link>
+						</li>
+						<li>
+							<Link to="/dashboard">Dashboard</Link>
+						</li>
+					</ul>
+				</div>
+				<div>
+					<ul className="flex justify-center items-center gap-3 font-lexend-exa">
+						<li>
+							<Link
+								className="link-outline-button"
+								to="/register"
+							>
+								Register
+							</Link>
+						</li>
+						<div className="w-0.5 h-8 bg-gray-500 rounded"></div>
+						<li>
+							<Link className="link-button" to="/login">
+								Login
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	);
+}
+
+export default Navbar;
