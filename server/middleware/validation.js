f5a14ddb3f5a58f76794c2d5037585b3730ab3ce
vehehgito <aryann2708@gmail.com>
2024-04-28 15:43:45 +0530

    Added email validation @server/middleware/validation.js

--- server/middleware/validation.js
+++ server/middleware/validation.js
@@ -1,15 +1,26 @@
 module.exports = function (req, res, next) {
     const { username__c, email__c, password__c, first_name__c, last_name__c, address__c, phone__c } = req.body;
 
+    function validEmail(userEmail) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
+    }
+
     if (req.path === "/register") {
         if (![username__c, email__c, password__c, first_name__c, last_name__c, address__c, phone__c].every(Boolean)) {
-            return res.json("Missing Credentials @vaidation.js in Register");
+            return res.json("Missing Credentials");
+        }
+        else if (!validEmail(email__c)) {
+            return res.json("Invalid Email");
         }
     }
     else if (req.path === "/login") {
-        if (![username__c, email__c, password__c, first_name__c, last_name__c, address__c, phone__c].every(Boolean)) {
-            return res.json("Missing Credentials @validation.js in Login");
+        if (![email__c, password__c].every(Boolean)) {
+            return res.json("Missing Credentials");
+        }
+        else if (!validEmail(email__c)) {
+            return res.json("Invalid Email");
         }
     }
+
     next();
 };
