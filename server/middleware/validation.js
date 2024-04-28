5a72e600187542cbec7a56db49ee54bf033a628e
vehehgito <aryann2708@gmail.com>
2024-04-28 23:10:38 +0530

    Fully functional registration with issues in login

--- server/middleware/validation.js
+++ server/middleware/validation.js
@@ -1,23 +1,23 @@
 module.exports = function (req, res, next) {
-    const { username__c, email__c, password__c, first_name__c, last_name__c, address__c, phone__c } = req.body;
+    const { username__c, inputEmail, inputPassword, first_name__c, last_name__c, address__c, phone__c } = req.body;
 
     function validEmail(userEmail) {
         return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
     }
 
     if (req.path === "/register") {
-        if (![username__c, email__c, password__c, first_name__c, last_name__c, address__c, phone__c].every(Boolean)) {
+        if (![username__c, inputEmail, inputPassword, first_name__c, last_name__c, address__c, phone__c].every(Boolean)) {
             return res.json("Missing Registration Credentials @validation.js");
         }
-        else if (!validEmail(email__c)) {
+        else if (!validEmail(inputEmail)) {
             return res.json("Invalid Email Credentials @validation.js");
         }
     }
     else if (req.path === "/login") {
-        if (![email__c, password__c].every(Boolean)) {
+        if (![inputEmail, inputPassword].every(Boolean)) {
             return res.json("Missing Login Credentials @validation.js");
         }
-        else if (!validEmail(email__c)) {
+        else if (!validEmail(inputEmail)) {
             return res.json("Invalid Login Email @validation.js");
         }
     }
